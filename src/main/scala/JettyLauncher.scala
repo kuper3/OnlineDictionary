5341031690bc4ed0cbb7151a7f745a77f4d7defb
kuper3 <qwerty_144@e-mail.ua>
2012-10-14 21:38:52 +0300

    fix JettyLauncher

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -4,6 +4,7 @@
  */
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet._
+import org.eclipse.jetty.webapp._
 import com.github.kuper3.onlinedictionary.MainScalatraServlet
 
 object JettyLauncher {
@@ -11,9 +12,15 @@ object JettyLauncher {
     val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
 
     val server = new Server(port)
-    val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+    //val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
 
+    //context.addServlet(classOf[MainScalatraServlet], "/");
+    val context = new WebAppContext()
+    context.setContextPath("/")
+    context.setResourceBase("src/main/webapp")
     context.addServlet(classOf[MainScalatraServlet], "/");
+
+    server.setHandler(context);
 
     server.start
     server.join
